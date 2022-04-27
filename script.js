db9acdf899f2f933f08a4a744d3320652bc79622
AndreyAvrach <andrey.avrach33@gmail.com>
2022-04-27 13:12:15 +0300

    first commit

--- script.js
+++ script.js
@@ -47,3 +47,42 @@ document.getElementById("insert_info").addEventListener("submit", function (even
   
   document.getElementById('datas').prepend(tr);
 });
+document.addEventListener('DOMContentLoaded', () => {
+
+  const getSort = ({ target }) => {
+      const order = (target.dataset.order = -(target.dataset.order || -1));
+      const index = [...target.parentNode.cells].indexOf(target);
+      const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+      const comparator = (index, order) => (a, b) => order * collator.compare(
+          a.children[index].innerHTML,
+          b.children[index].innerHTML
+      );
+      
+      for(const tBody of target.closest('table').tBodies)
+          tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+      for(const cell of target.parentNode.cells)
+          cell.classList.toggle('sorted', cell === target);
+  };
+  
+  document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+  
+});
+let ths = document.querySelectorAll('th');
+
+for (let i = 0; i < tds.length; i++) {
+    ths[i].addEventListener('click', function func() {
+        let input = document.createElement('input');
+        input.value = this.innerHTML;
+        this.innerHTML = '';
+        this.appendChild(input);
+        
+        let th = this;
+        input.addEventListener('blur', function() {
+            th.innerHTML = this.value;
+            th.addEventListener('click', func);
+        });
+        
+        this.removeEventListener('click', func);
+    });
+}
